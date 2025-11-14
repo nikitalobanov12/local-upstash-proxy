9baad7366e5cf4957bf20333c8e3200ccbac16fe
Nikita Lobanov <nikitaalobanovv@gmail.com>
2025-11-13 21:58:31 -0800

    update to server to include redis client

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,23 +1,46 @@
 package server
 
 import (
+	"context"
 	"fmt"
 	"net/http"
 	"os"
 	"strconv"
 	"time"
 
+	"github.com/redis/go-redis/v9"
+
 	_ "github.com/joho/godotenv/autoload"
 )
 
 type Server struct {
 	port int
+	rdb  *redis.Client
 }
 
 func NewServer() *http.Server {
 	port, _ := strconv.Atoi(os.Getenv("PORT"))
+	if port == 0 {
+		port = 8080
+	}
+
+	redisURL := os.Getenv("REDIS_URL")
+	if redisURL == "" {
+		redisURL = "localhost:6379"
+	}
+
+	rdb := redis.NewClient(&redis.Options{
+		Addr: redisURL,
+	})
+
+	ctx := context.Background()
+	if err := rdb.Ping(ctx).Err(); err != nil {
+		panic("Failed to connect to Redis: " + err.Error())
+	}
+
 	NewServer := &Server{
 		port: port,
+		rdb:  rdb,
 	}
 
 	// Declare Server config
